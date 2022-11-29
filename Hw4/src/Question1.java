a443d4b33db4151bf4d5a78150b3712ac70d828f
cliffort70 <79475902+cliffort70@users.noreply.github.com>
2022-11-29 17:45:50 -0500

    Update Question1.java

--- Hw4/src/Question1.java
+++ Hw4/src/Question1.java
@@ -1,14 +1,17 @@
 package SE320.Hw4.src;
-
 import java.time.MonthDay;
 import java.time.YearMonth;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Question1 {
     
     public static void main(String [] args){
         Day d = new Day(2002, 6, 4);
-        Day curD = new Day(2022, 11, 27);
+        Calendar cal = Calendar.getInstance();
+        Day curD = new Day(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        
+
         System.out.println(curD.daysFrom(d)); 
         
     }
